f4bf4ff6e7c400435909c0acc69b64175822fecb
Ali Sabri Taylan <alisabritaylan@gmail.com>
2018-07-02 22:28:21 +0300

    Add a new method for detecting seasonality using fourier transforms

--- R/ATA_SeasTest_FindFreq.r
+++ R/ATA_SeasTest_FindFreq.r
@@ -40,7 +40,7 @@ find.freq.fourier <- function(x)
 	pppx <- periodogram(x)
 	dddx = data.frame(freq=pppx$freq, spec=pppx$spec)
 	orderpppx = dddx[order(-dddx$spec),]
-	top5X = head(orderpppx, 2)
+	top5X = head(orderpppx, 5)
 	freq_all <- 1/top5X$freq
 	period <- sort(freq_all)
 	period <- period[period < 367]
